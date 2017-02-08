21a6b56eecf1087343434006f878cc99d34771ab
priyadarshinig <priyadarshinig97@gmail.com>
2017-02-08 18:04:30 +0530

    Prime number program

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int var1, i , flag=0;
+		System.out.println("Enter the Number:");
+		Scanner scan=new Scanner(System.in);
+		var1=scan.nextInt();
+		
+		for(i=2;i<=var1/2;i++)
+		{
+			if(var1%i==0)
+			{
+			flag=1;
+			break;
+			}
+		}
+
+		if(flag==1)
+			System.out.println("It is not a prime Number");
+		else
+			System.out.println("It is a prime Number");
+		}
+		
+	}
