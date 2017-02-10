0a45e2c968d2bd8f4b1925fc2f4217fa5373e4e2
priyadarshinig <priyadarshinig97@gmail.com>
2017-02-10 18:43:06 +0530

    setter method and getter method

---
+++ settergetter.java
@@ -0,0 +1,27 @@
+package Javafiles;
+
+public class settergetter {
+	 
+		private String name;
+		private int rollno;
+		private int totalmarks;
+		   
+		public String getName(){  
+		return name;  
+		}  
+		public int getRollno() {
+			return rollno;
+		}
+		public  int getTotalmarks(){
+			return totalmarks;
+		}
+		public void setName(String name){  
+		this.name=name;  
+		}  
+		public void setRollno(int rollno){
+			this.rollno=rollno;
+		}
+		public void setTotalmarks(int totalmarks){
+			this.totalmarks=totalmarks;
+			}
+}
---
+++ settergetter1.java
@@ -0,0 +1,18 @@
+package Javafiles;
+import Javafiles.settergetter;
+public class settergetter1 {
+
+	public static void main(String[] args) {
+		settergetter s=new settergetter();  
+		s.setName("priya");  
+		System.out.println("the name is:" + s.getName()); 
+		s.setRollno(123);  
+		System.out.println("the rollno is:" + s.getRollno());
+		s.setTotalmarks(99);  
+		System.out.println("the total mark is:" + s.getTotalmarks());
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
